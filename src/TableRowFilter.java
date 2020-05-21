cb0c2b56b1073e6c717ea65bbef5a425870fd941
Julia Kozłowska <jkozlowska5656@gmail.com>
2020-05-21 23:56:07 +0200

    Create TableRowFilter.java

---
+++ src/TableRowFilter.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.TableRowSorter;
+
+public class TableRowFilter {
+    public static JTextField create(JTextField jTextField, JTable jTable) {
+        jTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                update(e);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                update(e);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                update(e);
+            }
+
+            private void update(DocumentEvent e) {
+                String expression = jTextField.getText();
+                if(expression.trim().length() == 0) {
+                    ((TableRowSorter)jTable.getRowSorter()).setRowFilter(null);
+                } else {
+                    ((TableRowSorter)jTable.getRowSorter()).setRowFilter(RowFilter.regexFilter("(?i)" + expression));
+                }
+            }
+        });
+        return jTextField;
+    }
+}
